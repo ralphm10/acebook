5ed07be89c33f387562edcf6210627e67814d6ba
Student <student@Makerss-Air.broadband>
2020-09-04 11:47:50 +0100

    rubocop down to 16 offences

--- db/schema.rb
+++ db/schema.rb
@@ -11,67 +11,67 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20200903191306) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: :cascade do |t|
-    t.string 'poster'
-    t.text 'message'
-    t.bigint 'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_comments_on_post_id'
+  create_table "comments", force: :cascade do |t|
+    t.string "poster"
+    t.text "message"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table 'friend_requests', force: :cascade do |t|
-    t.bigint 'requestor_id'
-    t.bigint 'receiver_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['receiver_id'], name: 'index_friend_requests_on_receiver_id'
-    t.index ['requestor_id'], name: 'index_friend_requests_on_requestor_id'
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "requestor_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_friend_requests_on_receiver_id"
+    t.index ["requestor_id"], name: "index_friend_requests_on_requestor_id"
   end
 
-  create_table 'friendships', force: :cascade do |t|
-    t.bigint 'friend_a_id'
-    t.bigint 'friend_b_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['friend_a_id'], name: 'index_friendships_on_friend_a_id'
-    t.index ['friend_b_id'], name: 'index_friendships_on_friend_b_id'
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "friend_a_id"
+    t.bigint "friend_b_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_a_id"], name: "index_friendships_on_friend_a_id"
+    t.index ["friend_b_id"], name: "index_friendships_on_friend_b_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'message'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "message"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name', limit: 60
-    t.string 'last_name', limit: 60
-    t.string 'password_digest', limit: 60
-    t.string 'email', limit: 60
-    t.string 'provider'
-    t.string 'uid'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", limit: 60
+    t.string "last_name", limit: 60
+    t.string "password_digest", limit: 60
+    t.string "email", limit: 60
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider", limit: 120
+    t.string "uid", limit: 120
   end
 
-  create_table 'users_posts', id: false, force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'post_id'
-    t.index ['post_id'], name: 'index_users_posts_on_post_id'
-    t.index ['user_id'], name: 'index_users_posts_on_user_id'
+  create_table "users_posts", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_users_posts_on_post_id"
+    t.index ["user_id"], name: "index_users_posts_on_user_id"
   end
 
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'friend_requests', 'users', column: 'receiver_id'
-  add_foreign_key 'friend_requests', 'users', column: 'requestor_id'
-  add_foreign_key 'friendships', 'users', column: 'friend_a_id'
-  add_foreign_key 'friendships', 'users', column: 'friend_b_id'
-  add_foreign_key 'users_posts', 'posts'
-  add_foreign_key 'users_posts', 'users'
+  add_foreign_key "comments", "posts"
+  add_foreign_key "friend_requests", "users", column: "receiver_id"
+  add_foreign_key "friend_requests", "users", column: "requestor_id"
+  add_foreign_key "friendships", "users", column: "friend_a_id"
+  add_foreign_key "friendships", "users", column: "friend_b_id"
+  add_foreign_key "users_posts", "posts"
 end
